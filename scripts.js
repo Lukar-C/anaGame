fa242664f827ed102dd83d4f2343f7fdec4272e0
Luka Ravi <129634640+Lukar-C@users.noreply.github.com>
2023-04-02 23:51:10 -0300

    Project 1st Stage
    HTML: Improved to support CSS and JS modifications.
    CSS: Layout added and completed, Design - except image - added and completed, Dynamic Layout added and mostly completed,
    JS: Modifications to add dynamic layout has been added.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,20 @@
 let numberRandom = Math.floor(Math.random() * 100) + 1
 let numberTry = 0
 let number = 0
+
+let hiddenMensageResult = document.getElementById("mensageResult"); hiddenMensageResult.style.display = "none"
+let hiddenButtonTryAgain = document.getElementById("buttonTryAgain"); hiddenButtonTryAgain.style.display = "none"
+
 const numberCount = []
 
 function getNumber() {
     for(numberTry; numberTry < 10; numberTry++) {
         number = document.getElementById("numberInput").value
+        
         if(number < numberRandom) {
             numberCount.push(number)
             alert("O número em que Ana está pensando é maior")
-            document.edit.getElementById("pCount").innerHTML = `${numberCount}`
+            document.edit.getElementById("pColor2").innerHTML = `${numberCount}`
         }
         else if(number > numberRandom) {
             numberCount.push(number)
@@ -19,12 +24,12 @@ function getNumber() {
             alert("O número que você digitou já foi utilizado  vez")
         }
         else if(number == numberRandom) {
-            document.getElementsByClassName("mensageResult").innerHTML = ""
-            document.getElementById("p")
-
+            document.getElementsByClassName("mensageResult").innerHTML = "Parabéns, o número é:<span class='mensageResult' id='pColor1'>" + numberRandom + "</span></p>"
         }
         else {
-
+            document.getElementsByClassName("mensageResult").innerHTML = "Você não acertou, o número era: <span class='mensageResult' id='pColor1'>" + numberRandom + "</span>"
+            var hiddenMensageResult = hiddenMensageResult.style.display = "block"
+            var hiddenButtonTryAgain = hiddenButtonTryAgain.style.display = "block"
         }
     }
 }
