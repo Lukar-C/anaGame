7cf330f81c38000f6d416b68cbb912b3bace1966
Luka Ravi <129634640+Lukar-C@users.noreply.github.com>
2023-04-04 20:27:54 -0300

    Project Pre-Beta Version
    HTML: Improved to support CSS and JS modifications.
    
    CSS: Enhanced to solve some issues
    
    JS: Dynamic Layout - Enhanced to solve issues. Logic completed. All functions connected.
    
    System working!

--- scripts.js
+++ scripts.js
@@ -1,36 +1,16 @@
 /* variables and constants */
 let numberRandom = Math.floor(Math.random() * 100) + 1
 let numberTry = 0
-var number = document.getElementById("numberInput").value
+let number = 0
+let buttonTryAgain = document.getElementById("buttonTryAgain")
 
 /* CSS CONTROLLERS */
 let controllerImage = document.getElementById("biaPhoto")
 let controllerMensageResult = document.getElementById("mensageResult")
-let controllerButtonTryAgain = document.getElementById("buttonTryAgain")
 let controllerNumberInput = document.getElementById("numberInput")
+let controllerButtonTryAgain = document.getElementById("buttonTryAgain")
 
-const numberCount = [100, 90, 80] 
-var numberList = numberCount
-
-
-
-/**
- * document.getElementById("numberInput").addEventListener("keydown", function(event) {
-            if(event.key = 'Enter'){
-                var number = document.getElementById("numberInput")
-            }
-        })
-
-    * for(numberTry >= 0; numberTry < 10; numberTry++) {
-    }
-        */
-
-function getNumber(num) {
-        if(event.key === 'Enter') {
-            number = num.value
-            alert(num.value)
-        }
-    }
+const numberCount = [] 
 
 function controllerReferenceThink() {
     controllerImage.style.backgroundImage = "url('images/image(0).png')"
@@ -42,7 +22,7 @@ controllerReferenceThink()
 
 function controllerReferenceWin() {
     controllerImage.style.backgroundImage = "url('images/image(1).png')"
-    controllerMensageResult.innerHTML = "Parabéns, o número è: <span class='mensageResult' id='pColor1'>" + numberRandom + "</span>"
+    controllerMensageResult.innerHTML = "Parabéns, o número é: <span class='mensageResult' id='pColor1'>" + numberRandom + "</span>"
     controllerMensageResult.style.display = "block"
     controllerButtonTryAgain.style.display = "block"
     controllerNumberInput.style.display = "none"
@@ -56,15 +36,80 @@ function controllerReferenceLose() {
     controllerNumberInput.style.display = "none"
 }
 
+controllerButtonTryAgain.addEventListener("keypress", function(event){
+    if(event.key === "Enter") {
+        event.preventDefault()
+        document.getElementById("buttonTryAgain").click()
+    }
+})
+
+function reset() {
+    number = 0
+    numberTry = 0
+    numberCount.length = 0
+    document.getElementById("pColor2").innerHTML = " "
+    numberRandom = Math.floor(Math.random() * 100) + 1
+
+    controllerReferenceThink()
+}
+
+function getNumber(num) {
+    if(event.key === 'Enter') {
+        number = parseInt(num.value)
+        num.value = ""
+        submit()
+    }
+}
+
+function submit() {
+    if(number < 0 || number > 100) {
+        alert("O número precisa ser maior que zero e menor que 100")
+    }
+
+    else if(number > 0 && number <= 100) {
+        numberTry++
+        game()
+    }
+
+    else if (number == 0) {
+        reset()
+        alert("O jogo foi reiniciado")
+    }
+
+    else if(number == null) {
+        alert("Por Favor insira um número")
+    }
+
+    else {
+        alert("ERRO")
+    }
+}
+
+function game() {
+    if(numberTry < 10) {
+        compareNumber()
+    }
+    else if (numberTry == 10 && number == numberRandom) {
+        compareNumber()
+        controllerReferenceWin()
+    }
+    else if (numberTry == 10 && number != numberRandom) {
+        compareNumber()
+        controllerReferenceLose()
+    }
+    else {
+        alert("ERRO")
+    }
+}
+
 function compareNumber() {
-    getNumber()
-    
     if(numberCount.includes(number)) {
-        alert("O número que você digitou já foi utilizado  vez")
+        alert("O número que você digitou já foi utilizado uma vez")
+        numberTry--
     }
         
     else if(number < numberRandom) {
-        numberCount.push(number.join('-'))
+        numberCount.push(number)
         alert("O número em que Ana está pensando é maior")
         document.getElementById("pColor2").innerHTML = numberCount.join(" - ")
     }
@@ -76,24 +121,11 @@ function compareNumber() {
     }
         
     else if(number == numberRandom) {
+        numberCount.push(number)
+        document.getElementById("pColor2").innerHTML = numberCount.join(" - ")
         controllerReferenceWin()
-    }
-    else {
-        alert(ERRO)
-    }
-}
-
-function game() {
-    controllerReferenceThink()
-
-    if(numberTry < 10) {
-        compareNumber()
-    }
-    else if (numberTry == 10) {
-        controllerReferenceLose()
     }
     else {
         alert("ERRO")
     }
 }
-
